1ad32975dd422de5b13291522a4ba1612618f619
supersem <xsupersemx@gmail.com>
2019-03-25 17:41:23 +0200

    Unit test question controller

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
+  gem 'slim-rails'
 end
 
 group :development do
@@ -58,6 +60,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'shoulda-matchers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,124 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+
+    before do
+      get :index
+    end
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(@questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: {id: question} }
+
+    it 'assigns the requested question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'GET :create' do
+    context 'with valid attributes' do
+      it 'saves the new question in the db' do
+        expect { post :create, params: {  question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirect to show view' do
+        post :create, params: { question: attributes_for(:question)}
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save' do
+        expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question, :count)
+      end
+
+      it 'rerenders new view' do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+  describe 'PATCH #update' do
+    context 'valid attributes' do
+      it 'assigns the requested question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+
+      it 'redirect to the updated question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(response).to redirect_to question
+      end
+    end
+    context 'invalid attributes' do
+      before { patch :update, params: { id: question, question: { title: 'new title', body: nil } } }
+      it 'does not change attributes' do
+        question.reload
+        expect(question.title).to eq "MyString"
+        expect(question.body).to eq "MyText"
+      end
+
+      it 'rerender edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destoy' do
+    it 'delete question' do
+      question.reload
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index view' do
+      delete :destroy, params: { id: question}
+      expect(response).to redirect_to question_path
+    end
+  end
+end

80ee7436de9f62a6c11677a426e687bd06bf761a
supersem <xsupersemx@gmail.com>
2019-04-10 10:26:12 +0300

    Question accepts nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers
   has_many :attachments
-  
+
   validates :title, :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 end
